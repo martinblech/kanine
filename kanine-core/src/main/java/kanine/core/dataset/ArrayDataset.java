1f9bc0e4f297934923492f0e414e42ca62a045d7
Martin Blech <martinblech@gmail.com>
2013-02-14 00:46:57 -0300

    array dataset and test

---
+++ kanine-core/src/main/java/kanine/core/dataset/ArrayDataset.java
@@ -0,0 +1,24 @@
+package kanine.core.dataset;
+
+import kanine.core.accumulator.BestResultsAccumulator;
+import kanine.core.scorer.Scorer;
+
+public class ArrayDataset implements Dataset {
+
+    private final float[] data;
+    private final int vectorLength;
+    private final int size;
+
+    public ArrayDataset(float[] data, int vectorLength, int size) {
+        this.data = data;
+        this.vectorLength = vectorLength;
+        this.size = size;
+    }
+
+    public void apply(Scorer scorer, BestResultsAccumulator accum) {
+        for (int i = 0; i < size; i++) {
+            float score = scorer.inverseScore(data, i * vectorLength);
+            accum.accumulate(i, score);
+        }
+    }
+}
