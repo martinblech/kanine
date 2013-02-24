ff8bff04286f29a3ca3a530324586dd69336feaf
Martin Blech <martinblech@gmail.com>
2013-02-24 05:04:31 -0300

    added MIT license

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ kanine
 ======
 
 k-NN search
+
+License
+-------
+
+`kanine` is MIT-licensed. Plese read the `LICENSE` file.
--- kanine-core/src/main/java/kanine/core/package-info.java
+++ kanine-core/src/main/java/kanine/core/package-info.java
@@ -2,7 +2,5 @@
  * Core <i>k-NN search</i> classes: {@link kanine.core.Dataset}, {@link
  * kanine.core.Distance}, {@link kanine.core.Scorer} and {@link
  * kanine.core.BestResultsAccumulator}.
- *
- * @author Martin Blech
  */
 package kanine.core;
